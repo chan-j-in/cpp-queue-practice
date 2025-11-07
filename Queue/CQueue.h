cae3525e4fdda9add9044f8bd67fb6c4165ff62e
Jeon <genius8111@naver.com>
2025-11-07 13:12:00 +0900

    queue push, pop 구현

--- Queue/CQueue.h
+++ Queue/CQueue.h
@@ -1,4 +1,83 @@
+#pragma once
 
-class CQueue
+template <typename T>
+class CQueueNode
 {
+private:
+	CQueueNode() : next(nullptr), prev(nullptr) {}
+	~CQueueNode() {}
+
+private:
+	CQueueNode<T>* next;
+	CQueueNode<T>* prev;
+	T data;
+
+private:
+	template <typename U>
+	friend class CQueue;
 };
+
+template <typename T>
+class CQueue {
+private:
+	typedef CQueueNode<T> NODE;
+	typedef CQueueNode<T>* PNODE;
+
+private:
+	PNODE front;
+	PNODE rear;
+	unsigned int size;
+
+public:
+	CQueue() {
+		front = new NODE;
+		rear = new NODE;
+		size = 0;
+
+		front->next = rear;
+		front->prev = nullptr;
+
+		rear->prev = front;
+		rear->next = nullptr;
+	}
+	~CQueue() {
+		PNODE node = front;
+		while (node) {
+			PNODE next = node->next;
+			delete node;
+			node = next;
+		}
+	}
+
+public:
+	void push(const T& data) {
+		PNODE node = new NODE;
+		node->data = data;
+
+		node->next = rear;
+		node->prev = rear->prev;
+
+		rear->prev->next = node;
+		rear->prev = node;
+
+		size++;
+	}
+
+	T pop() {
+		if (isEmpty()) return T{};
+
+		PNODE node = front->next;
+		T data = node->data;
+
+		front->next = node->next;
+		node->next->prev = front;
+		delete node;
+
+		size--;
+
+		return data;
+	}
+
+	bool isEmpty() { return size == 0; }
+};
+
