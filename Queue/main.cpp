ca43952023cd1cbeb008eccaca1901f62e635795
Jeon <genius8111@naver.com>
2025-11-07 13:32:24 +0900

    push, pop main구현

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include "CQueue.h"
 
+using namespace std;
+
 int main() {
+
+	CQueue<int> queueInt;
+
+	for (int i = 0; i < 100; i++) {
+		queueInt.push(i + 1);
+	}
+
+	while(!queueInt.isEmpty()) {
+		cout << queueInt.pop() << endl;
+	}
 
 	return 0;
 }
